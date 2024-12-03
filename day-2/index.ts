e7a3e2c16b72bfdeca4885550dc480c03ce1c54f
Simeon Kostadinov <simeon.kostadinov0141@gmail.com>
2024-12-03 20:35:27 +0100

    sync commit

--- day-2/index.ts
+++ day-2/index.ts
@@ -1,6 +1,11 @@
 import {getInput} from "../composables";
 
-function checkReport(line: string): boolean | number[]{
+function removeElement(index: number, elementsArr: number[]){
+  console.log(elementsArr, [...elementsArr].splice(index, 1))
+  return [...elementsArr].splice(index, 1).join(' ')
+}
+
+function checkReport(line: string, blockRecursion: boolean = false): boolean | number[]{
   const elements = line.split(' ').map(element => parseInt(element));
   let i = 0;
   let lastDistance: number | undefined = undefined;
@@ -9,25 +14,42 @@ function checkReport(line: string): boolean | number[]{
     if (i == elements.length - 1) break;
     const currentDistance = elements[i] - elements[i+1];
     if (currentDistance === 0){
+      if(!blockRecursion){
+        const res = checkReport(removeElement(i, elements), true)
+        if (res === true) return true
+      }
       wrongPositions.push(i);
       i++
       lastDistance = currentDistance
       continue
     }
     if (lastDistance === undefined) lastDistance = currentDistance
+
     if (Math.abs(currentDistance) > 3) {
+      if(!blockRecursion){
+        const res = checkReport(removeElement(i, elements), true)
+        if (res === true) return true
+      }
       wrongPositions.push(i);
       i++
       lastDistance = currentDistance
       continue
     }
     if ((lastDistance > 0 && currentDistance < 0)) {
+      if(!blockRecursion){
+        const res = checkReport(removeElement(i, elements), true)
+        if (res === true) return true
+      }
       wrongPositions.push(i);
       i++
       lastDistance = currentDistance
       continue
     }
     if (lastDistance < 0 && currentDistance > 0) {
+      if(!blockRecursion){
+        const res = checkReport(removeElement(i, elements), true)
+        if (res === true) return true
+      }
       wrongPositions.push(i);
       i++
       lastDistance = currentDistance
@@ -36,8 +58,7 @@ function checkReport(line: string): boolean | number[]{
     lastDistance = currentDistance
     i++;
   }
-  if (wrongPositions.length === 0) return true
-  return wrongPositions
+  return wrongPositions.length === 0;
 }
 
 export function day2part1(): number {
@@ -47,7 +68,7 @@ export function day2part1(): number {
   let result: number = 0;
 
   lines.forEach((line) => {
-    const res = checkReport(line);
+    const res = checkReport(line, true);
     if (res === true) result++
   })
 
@@ -61,21 +82,8 @@ export function day2part2(): number {
   let result: number = 0;
 
   lines.forEach((line) => {
-    const checkRes = checkReport(line);
-    console.log(line, checkRes)
-    if (checkRes === true) result++;
-    else {
-      const elements = line.split(' ').map(element => parseInt(element));
-      let foundSolution = false;
-      (checkRes as number[]).forEach((pos, index) => {
-        const newLine = [...elements].splice(pos,1).join(' ')
-        const checkRes = checkReport(newLine);
-        if (checkRes === true) {
-          foundSolution = true
-        }
-      })
-      if (foundSolution) result++
-    }
+    const res = checkReport(line);
+    if (res === true) result++
   })
 
   return result;
