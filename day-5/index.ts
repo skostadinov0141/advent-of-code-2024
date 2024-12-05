67e7a526e2907fcf5987c012b7c3d5019f23dab4
Simeon Kostadinov <simeon.kostadinov0141@gmail.com>
2024-12-05 21:44:07 +0100

    completed day 5 part 1

---
+++ day-5/index.ts
@@ -0,0 +1,83 @@
+import {getInput} from "../composables";
+
+interface InputParts {
+  rules: number[][],
+  printers: string[],
+}
+
+function parseInput(): InputParts{
+  const input = getInput(5);
+  const inputSplit = input.split('\n\n')
+  return {
+    rules: inputSplit[0].split('\n').map(item => item.split('|').map(item1 => parseInt(item1))),
+    printers: inputSplit[1].split('\n'),
+  }
+}
+
+function generateRuleRegex(rule: number[]){
+  return new RegExp(String.raw`((?<=${rule[1]}).*?${rule[0]})`, 'gm')
+}
+
+function getApplicableRules(rules: number[][], printer:string){
+  const applicableRules: number[][][] = []
+  const printerList = printer.split(',').map(item => parseInt(item))
+
+  printerList.forEach(printerItem => {
+    applicableRules.push(
+      rules.filter(rule => {
+        return rule[0] == printerItem
+      })
+    )
+  })
+
+  return applicableRules
+}
+
+function testPrinter(applicableRule: number[][][], printer: string){
+  const printerElements = printer.split(',').map(item => parseInt(item))
+  const startIndex = printerElements.length - 1
+
+  for (let i = startIndex; i > 0; i--) {
+    const elementRelatedRules = applicableRule[i]
+    for (let rule of elementRelatedRules){
+      const ruleRegex = generateRuleRegex(rule)
+      const valid = printer.match(ruleRegex) === null
+      if (!valid) return false// console.log(`Rule ${rule} is applicable to ${printer}`)
+      // else console.log(`Rule ${rule} is NOT applicable to ${printer}`)
+    }
+  }
+  return true
+}
+
+export function day5part1(){
+  const parsedInput = parseInput()
+
+  // array 1 = printer number
+  // array 2 = printer element
+  // array 3 = applicable rule to element
+  // array 4 = rule element
+  const applicableRulesList: number[][][][]
+    = parsedInput.printers.map(printer => getApplicableRules(parsedInput.rules, printer))
+
+  const correctPrinter: string[] = []
+
+  for (let i = 0; i < applicableRulesList.length; i++) {
+    const printer = parsedInput.printers[i]
+    const applicableRules = applicableRulesList[i]
+    const result = testPrinter(applicableRules, printer)
+    if(result) correctPrinter.push(printer)
+  }
+
+  let finalResult = 0
+
+  correctPrinter.forEach(printer => {
+    const printerElements = printer.split(',').map(item => parseInt(item))
+    finalResult += printerElements[Math.round((printerElements.length - 1) / 2)]
+  })
+
+  return finalResult
+}
+
+export function day5part2(){
+
+}
--- index.ts
+++ index.ts
@@ -2,6 +2,7 @@ import {day1part1, day1part2} from "./day-1";
 import {day2part1, day2part2} from "./day-2";
 import {day3part1, day3part2} from "./day-3";
 import {day4part1, day4part2} from "./day-4";
+import {day5part1, day5part2} from "./day-5";
 
 // Day 1
 console.log('========== Day 1 ==========')
@@ -22,3 +23,8 @@ console.log(`Answer to D3P2 is ${day3part2()}`)
 console.log('\n\n========== Day 4 ==========')
 console.log(`Answer to D4P1 is ${day4part1()}`)
 console.log(`Answer to D4P2 is ${day4part2()}`)
+
+// Day 5
+console.log('\n\n========== Day 5 ==========')
+console.log(`Answer to D5P1 is ${day5part1()}`)
+console.log(`Answer to D5P2 is ${day5part2()}`)
